8f1489cadeaed28ca9ae2967c7549145cbabdcb9
Caio Leone Fonseca Casanova Soeiro <caio_leone@hotmail.com>
2026-02-19 21:09:28 -0300

    fix: update main

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
+	var valor_em_brl float32
+	//var moeda_destino string
+
 	fmt.Println("Bem Vindo, Aventureiro! Vamos converter suas moedas!")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("./convert [valor_em_brl] [moeda_destino]\n")
+	scanner.Scan()
+	fmt.Sscanf(scanner.Text(), "%f", &valor_em_brl)
+	fmt.Scanln(&valor_em_brl)
+
 }
